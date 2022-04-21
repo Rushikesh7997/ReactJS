286baccea65bd904c8d9f4e63d5e345c503ab30b
Rushikesh7997 <pardhikar26@gmail.com>
2022-04-22 02:10:38 +0530

    clear

--- mobile/src/components/context/cartContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const createContext = createContext();
-
-export const cartContextProvider = ({ childern }) =>{
-
-    return <cartContext.Provider value = {0}></cartContext.Provider>
-};
-
