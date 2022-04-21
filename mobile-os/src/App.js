d9bac2232fec2f90cd74fde38b83589a96a63a8a
Rushikesh7997 <pardhikar26@gmail.com>
2022-04-22 00:28:10 +0530

    mobile-os completed

--- mobile-os/src/App.js
+++ mobile-os/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const food = "Pizza";
+  const age = "19";
+  const arr1 = ["Android", "Blackberry", "iphone", "Windows Phone"];
+  const arr2 = ["Samsung", "HTC", "Micromax", "Apple"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ul>
+          <h2>Mobile Operating System</h2>
+          {arr1.map((e) => {
+            return <li><Skill arr1={e}/> </li>;
+          })}
+        </ul>
+        <ul>
+          <h2>Mobile Manufacturers</h2>
+          {arr2.map((e) => {
+            return <li><Skillss arr2={e}/> </li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
 
+function Skill (props){
+  return <p> {props.arr1} </p>
+}
+function Skillss (props){
+  return <p> {props.arr2} </p>
+}
+
 export default App;
+
+
+
+{/* <li>{e}</li> */}
