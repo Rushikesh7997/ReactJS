5341def107e99245b0a92e8957a2d0a0267a7fe7
Rushikesh7997 <pardhikar26@gmail.com>
2022-04-25 14:40:55 +0530

    c3 start

--- react-counter/src/App.jsx
+++ react-counter/src/App.jsx
@@ -3,18 +3,39 @@ import "./App.css";
 import { Category } from "./components/category";
 
 function App() {
-  const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(0);
+
+  // 1. first method 
+  // const handleAdd = () => {
+  //   if (counter >= 10) {
+  //     return;
+  //   }
+  //   setCounter(counter + 1);
+  // };
+
+  // const handleSub = () => {
+  //   if (counter == 0) {
+  //     return;
+  //   }
+  //   setCounter(counter - 1);
+  // };
+
+  // 2. second method 
+  const handleChange = (value) =>{
+    // conditions 
+    setCounter(counter + value);
+  };
+
+  if (counter >= 10){
+    return <h4>Counter reaches maximum value</h4>
+  }
 
   return (
     <div className="App">
       <h3>Counter: {counter}</h3>
-      <button
-        onClick={() => {
-          setCounter(counter+1);
-        }}
-      >
-        Add 1
-      </button>
+      <button onClick={()=>{handleChange(1)}}>Add 1</button>
+      <button onClick={()=>{handleChange(-1)}}>Sub 1</button>
+      <div className="">Number is {counter % 2 === 0 ? "Even" : "Odd"} </div>
     </div>
   );
 }
@@ -46,7 +67,3 @@ function App() {
 }
 export default App;
  */
-
-
-
-
