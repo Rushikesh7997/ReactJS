604aaac0b14e881d5c67aeea877b8875c8938771
Rushikesh7997 <pardhikar26@gmail.com>
2022-05-01 17:42:53 +0530

    form update

---
+++ react_form/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Employee } from "./components/employe";
+
+function App() {
+  return (
+    <div className="App">
+      <Employee />
+    </div>
+  );
+}
+
+export default App;
